bc02f7c781747ba297fc638dd59222721a1968fc
hiro246 <roki_stereo@yahoo.co.jp>
2020-03-06 18:12:18 +0900

    3/6

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -9,26 +9,59 @@ GAME RULES:
 
 */
 
-var scores, roundscore, activePlayer, dice;
+var scores, roundScore, activePlayer, dice;
 
 scores = [0,0];
-roundscore = 0;
-activePlayer = 0;
+roundScore = 0;
+activePlayer = 1;
 
-dice = Math.floor(Math.random() * 6) + 1;
-
-document.querySelector('#current-' + activePlayer).textContent = dice;
-// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
-
-var x = document.querySelector('#score-0').textContent;
-console.log(x);
+// dice = Math.floor(Math.random() * 6) + 1;
 
 document.querySelector('.dice').style.display = 'none';
 
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
 
+document.querySelector('.btn-roll').addEventListener('click', function() {
+
+    // 1. Random number
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+    //2.Display the result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
 
 
+    //3. Update the round score IF the rolled number was NOT a 1
+    if (dice !== 1) {
+        //Add score
+        roundscore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+        //Next player
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        document.querySelector('.dice').syle.display = 'none'
+
+    }
+
+});
+
+
+// document.querySelector('#current-' + activePlayer).textContent = dice;
+// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+// var x = document.querySelector('#score-0').textContent;
 
 
 
