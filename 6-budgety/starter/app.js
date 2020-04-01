25826feca582e25cf4411887cb64d32c7bd1939b
hiro246 <roki_stereo@yahoo.co.jp>
2020-04-01 18:11:56 +0900

    4/1(水) 78まで

---
+++ 6-budgety/starter/app.js
@@ -0,0 +1,34 @@
+var budgetController = (function() {
+
+    var x = 23;
+
+    var add = function(a) {
+        return x + a ;
+    }
+
+    return {
+        publicTest: function(b) {
+            console.log(add(b));
+        }
+    }
+
+})();
+
+var UIController = (function() {
+
+    // Some code
+
+})();
+
+var controller = (function(budgetCtrl, UICtrl) {
+
+    budgetController.publicTest(5);
+
+    return {
+        anotherPublic: function() {
+            console.log(z);
+        }
+    }
+
+})();
+
